fcba80addd9bbb108ece80cf908f7a2fa8feffd5
Chaika9 <chaika9.perso@gmail.com>
2024-05-30 09:28:01 +0900

    Edit Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,7 @@ export function Footer() {
                 <div
                     className="mx-auto flex w-[90%] lg:w-[95%] xl:w-[80%] flex-col space-y-10 py-6 lg:flex-row lg:items-center lg:justify-between lg:space-y-0 lg:py-0">
                     <div className="flex flex-col gap-2">
-                        <p className="lg:text-[0.8rem] text-[#9c9c9c] max-w-2xl italic">Lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit. Fusce quam mi, semper non nisi gravida, viverra accumsan
-                            tortor. Maecenas vel erat blandit, fringilla magna non, molestie eros.</p>
+                        <p className="lg:text-[0.8rem] text-[#9c9c9c] max-w-2xl italic">@2024 Chaika9 All Rights Reserved.</p>
                     </div>
                 </div>
             </div>
